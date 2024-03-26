6d20043287a8468287ac20f7f2a6c3e05fcdc7f9
JobKoech610 <jobkoech610@gmail.com>
2024-03-27 00:47:17 +0300

    console test

--- console.py
+++ console.py
@@ -150,6 +150,62 @@ class HBNBCommand(cmd.Cmd):
         print(obj)
         storage.save()
 
+    def default(self, line):
+        """handle class commands"""
+        ln = line.split('.', 1)
+        if len(ln) < 2:
+            print('*** Unknown syntax:', ln[0])
+            return False
+        clsname, line = ln[0], ln[1]
+        if clsname not in list(self.__classes.keys()):
+            print('*** Unknown syntax: {}.{}'.format(clsname, line))
+            return False
+        ln = line.split('(', 1)
+        if len(ln) < 2:
+            print('*** Unknown syntax: {}.{}'.format(clsname, ln[0]))
+            return False
+        mthname, args = ln[0], ln[1].rstrip(')')
+        if mthname not in ['all', 'count', 'show', 'destroy', 'update']:
+            print('*** Unknown syntax: {}.{}'.format(clsname, line))
+            return False
+        if mthname == 'all':
+            self.do_all(clsname)
+        elif mthname == 'count':
+            print(self.count_class(clsname))
+        elif mthname == 'show':
+            self.do_show(clsname + " " + args.strip('"'))
+        elif mthname == 'destroy':
+            self.do_destroy(clsname + " " + args.strip('"'))
+        elif mthname == 'update':
+            lb, rb = args.find('{'), args.find('}')
+            d = None
+            if args[lb:rb + 1] != '':
+                d = eval(args[lb:rb + 1])
+            ln = args.split(',', 1)
+            objid, args = ln[0].strip('"'), ln[1]
+            if d and type(d) is dict:
+                self.handle_dict(clsname, objid, d)
+            else:
+                from shlex import shlex
+                args = args.replace(',', ' ', 1)
+                ln = list(shlex(args))
+                ln[0] = ln[0].strip('"')
+                self.do_update(" ".join([clsname, objid, ln[0], ln[1]]))
+
+    def handle_dict(self, clsname, objid, d):
+        """handle dictionary update"""
+        for k, v in d.items():
+            self.do_update(" ".join([clsname, objid, str(k), str(v)])) 
+
+    @staticmethod
+    def count_class(clsname):
+        """count number of objects of type clsname"""
+        c = 0
+        for k, v in storage.all().items():
+            if type(v).__name__ == clsname:
+                c += 1
+        return (c)           
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
---
+++ tests/test_console.py
@@ -0,0 +1,89 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from io import StringIO
+from console import HBNBCommand
+from models import storage
+
+
+class TestHBNBCommand(unittest.TestCase):
+
+    def setUp(self):
+        self.console = HBNBCommand()
+
+    def tearDown(self):
+        self.console = None
+
+    def test_emptyline(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.emptyline()
+            self.assertEqual(fake_out.getvalue(), '')
+
+    def test_do_quit(self):
+        self.assertTrue(self.console.do_quit(''))
+
+    def test_help_quit(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.help_quit()
+            self.assertIn("Quit command to exit the program", fake_out.getvalue())
+
+    def test_do_create(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_create("BaseModel")
+            output = fake_out.getvalue().strip()
+            self.assertTrue(len(output) > 0)
+
+    def test_do_show(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_show("")
+            self.assertIn("** class name missing **", fake_out.getvalue())
+
+    def test_do_destroy(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_destroy("")
+            self.assertIn("** class name missing **", fake_out.getvalue())
+
+    def test_do_all(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_all("")
+            self.assertIn("** class name missing **", fake_out.getvalue())
+
+    def test_do_update(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_update("")
+            self.assertIn("** class name missing **", fake_out.getvalue())
+
+    def test_default_unknown_syntax(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.default("UnknownClass.unknownMethod()")
+            self.assertIn("*** Unknown syntax:", fake_out.getvalue())
+
+    def test_default_all(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.default("BaseModel.all()")
+            self.assertIn("[]", fake_out.getvalue())
+
+    def test_default_count(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.default("BaseModel.count()")
+            self.assertIn("0", fake_out.getvalue())
+
+    def test_default_show(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.default("BaseModel.show('123')")
+            self.assertIn("** instance id missing **", fake_out.getvalue())
+
+    def test_default_destroy(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.default("BaseModel.destroy('123')")
+            self.assertIn("** instance id missing **", fake_out.getvalue())
+
+    def test_default_update(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.default("BaseModel.update('123', {'name': 'test'})")
+            self.assertIn("** instance id missing **", fake_out.getvalue())
+
+
+if __name__ == '__main__':
+    unittest.main()
+    with patch('sys.stdout', new=StringIO()) as f:
+        HBNBCommand().onecmd("help show")
